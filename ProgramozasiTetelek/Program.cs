6510959ae9e9743258d5c1077380f3545f1854e2
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-09-11 10:46:01 +0200

    Összegzés tétele

--- ProgramozasiTetelek/Program.cs
+++ ProgramozasiTetelek/Program.cs
@@ -8,7 +8,7 @@ namespace ProgramozasiTetelek
 {
     class Program
     {
-        static int[] tomb = new int[] { 12, 7, 2, 4, 45, 88, 1, 8, 95, 50 };
+        static int[] tomb = new int[] { 12, 7, 2, 4, 45, 99, 1, 8, 95, 50 };
         static void TombKiiras()
         {
             for (int i = 0; i < tomb.Length; i++)
@@ -16,9 +16,22 @@ namespace ProgramozasiTetelek
                 Console.Write($"{tomb[i]},");
             }
         }
+        static void Osszegzes()
+        {
+            int osszeg = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                osszeg += tomb[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine("Összegzés tétele:");
+            Console.WriteLine(osszeg);
+        }
         static void Main(string[] args)
         {
             TombKiiras();
+            
+            Osszegzes();
             Console.ReadKey();
         }
     }
--- README.md
+++ README.md
@@ -5,7 +5,11 @@
  - A tömb elemei:
    - 12, 7, 2 ,4, 45, 88, 1, 8, 95, 50
   - A tömb az összes metódusban látható legyen
-  - TombKiiras() metódus létrehozása
+  - TombKiiras metódus létrehozása
     - Nem ad vissza értéket
     - Nincs paramétere
     - A tömb elemeit vesszõvel elválasztva a konzolra kiírja.
+## Összegzés
+  - Osszegzes metódus létrehozása
+    - Összegzés tétele a tömbön
+    - Eredmény kiírása a konzolra
